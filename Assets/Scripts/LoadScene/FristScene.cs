2323b9ede60ada4b2ed87404e900f31d555fe595
SinHoo99 <tjdqls991218@gmail.com>
2025-03-06 15:59:38 +0900

    LoadingScene추가

--- Assets/Scripts/LoadScene/FristScene.cs
+++ Assets/Scripts/LoadScene/FristScene.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FristScene : MonoBehaviour
 {
-    public void OnClickLoadBtn()
+    public void StartGame()
     {
+        SceneManager.LoadScene("LoadingScene"); // 로딩 씬으로 이동
     }
 }
--- Assets/Scripts/LoadScene/SceneLoadManager.cs
+++ Assets/Scripts/LoadScene/SceneLoadManager.cs
@@ -15,48 +15,24 @@ public class SceneLoadManager : Singleton<SceneLoadManager>
 
     private void Start()
     {
-        StartCoroutine(LoadSceneAsync());
+        StartCoroutine(FakeLoading());
     }
 
-    // 씬 이동 시 해당 메서드 사용
-    public static void LoadScene(int sceneNum)
+    private IEnumerator FakeLoading()
     {
-        _nextSceneNumber = sceneNum;
-        SceneManager.LoadScene(1);
-    }
-
-    private IEnumerator LoadSceneAsync()
-    {
-        // 로딩 UI 활성화
         LoadingUI.SetActive(true);
+        float progress = 0f;
 
-        yield return null;
-
-        // 해당 씬을 비동기적으로 로드
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(_nextSceneNumber);
-        // 씬 로딩이 끝나면 자동으로 넘어갈지 선택
-        asyncLoad.allowSceneActivation = false;
-
-        // 로딩 진행 상황 업데이트
-        while (!asyncLoad.isDone)
+        // 2초 동안 진행 바가 천천히 증가
+        while (progress < 1f)
         {
-            // 진행률을 0 ~ 1 사이 값으로 변환
-            #region // 설명
-            // AsyncOperation.progress 값은 0 ~ 1 까지의 범위를 가진다.
-            // 0 ~ 0.9는 로딩 진행 상황, 0.9 ~ 1은 씬이 활성화되는 과정이므로,
-            // 0.9로 나눠주면 진행 상황을 0% ~ 100%까지 모두 반영할 수 있다.
-            #endregion
-            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+            progress += Time.deltaTime * 0.5f; // 2초 동안 100% 도달
             ProgressBar.value = progress;
-            ProgressText.text = $"Loading...{Mathf.FloorToInt(progress * 100)}%";
-
-            // 로딩이 완료되면 씬 활성화
-            if (asyncLoad.progress >= 0.9f)
-            {
-                asyncLoad.allowSceneActivation = true;
-            }
-
+            ProgressText.text = $"Loading... {Mathf.FloorToInt(progress * 100)}%";
             yield return null;
         }
+
+        // 로딩 완료 후 게임 씬으로 이동
+        SceneManager.LoadScene("SampleScene");
     }
 }
